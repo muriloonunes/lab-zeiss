e8400e85f890a94fcf640aa073f64980dbaa1b0d
Murilo <57834900+muriloonunes@users.noreply.github.com>
2026-08-25 12:07:11 -0300

    refatora seção hero

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './home.html',
   styleUrl: './home.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Home {
   items = [1, 2, 3, 4, 5];
 }
+
